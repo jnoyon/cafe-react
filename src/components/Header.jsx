8db15a868eccf03e924215645b3b3598b8b14690
jnoyon <jihadur51@gmail.com>
2024-10-21 22:11:22 +0600

    tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+ const Header = () => {
+    return (
+        <>
+            <div className="header flex gap-5 justify-between py-5 border-b border-solid border-gray-300">
+                <h1 className="font-bold text-3xl"> Knowledge Cafe </h1>
+                <div className="user">
+                    <span> <img src="" alt="Image" /> </span>
+                </div>
+            </div>
+        </>
+    )
+ }
+ export default Header;
