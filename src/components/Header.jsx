add72e8336a69e80f91330bc40c9ba0447a7f2d8
jnoyon <jihadur51@gmail.com>
2024-10-22 00:09:39 +0600

    build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
  const Header = () => {
     return (
         <>
-            <div className="header flex gap-5 justify-between py-5 border-b border-solid border-gray-300">
+            <div className="header flex gap-5 justify-between py-3 border-b border-solid border-gray-300">
                 <h1 className="font-bold text-3xl"> Knowledge Cafe </h1>
                 <div className="user">
-                    <span> <img src="" alt="Image" /> </span>
+                    <span> <img src="https://i.ibb.co/vwGwnhT/boy2.jpg" alt="Image" className="w-14 h-14 p-1 border border-solid border-gray-300 rounded-full" /> </span>
                 </div>
             </div>
         </>
